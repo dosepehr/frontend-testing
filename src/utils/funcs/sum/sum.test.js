6396937d472c075a620e183bf0cc7e229be8b95e
dosepehr <dosepehr@gmail.com>
2025-05-02 20:22:50 +0330

    add testing objects

--- src/utils/funcs/sum/sum.test.js
+++ src/utils/funcs/sum/sum.test.js
@@ -1,5 +1,12 @@
 import { describe, expect, it } from 'vitest';
 import { sum } from './sum';
+import { v4 as id } from 'uuid';
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.id = 'person-' + id();
+    }
+}
 
 describe('sum tests', () => {
     it('should return 4', () => {
@@ -14,3 +21,38 @@ describe('sum tests', () => {
         expect(() => sum(2, 'sepehr')).toThrowError();
     });
 });
+
+describe('objects', () => {
+    it('should equal', () => {
+        expect({ foo: 'bar' }).toEqual({ foo: 'bar' });
+    });
+    it('should not equal', () => {
+        expect({ a: 1 }).not.toEqual({ a: 2 });
+    });
+    it('should ignore undefiend vars', () => {
+        expect({ a: 1, b: undefined }).toEqual({ a: 1 });
+    });
+    it('should not ignore undefiend vars', () => {
+        expect({ a: 1, b: undefined }).not.toStrictEqual({ a: 1 });
+    });
+    it('should ignore different instances', () => {
+        expect(new Person('sepehr')).toEqual({
+            id: expect.stringContaining('person-'),
+            name: 'sepehr',
+        });
+    });
+    it('should not ignore undefiend vars', () => {
+        expect(new Person('sepehr')).not.toStrictEqual({
+            name: 'sepehr',
+            id: expect.stringContaining('person-'),
+        });
+    });
+    it('should should equal a part of that object', () => {
+        expect(new Person('sepehr')).toEqual(
+            expect.objectContaining({
+                name: 'sepehr',
+            })
+        );
+    });
+});
+
