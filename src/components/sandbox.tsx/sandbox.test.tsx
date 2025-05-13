ad3de7afeb954fd678c637d4fbc61f14d6671c5e
dosepehr <dosepehr@gmail.com>
2025-05-13 16:36:33 +0330

    add more sandbox test

--- src/components/sandbox.tsx/sandbox.test.tsx
+++ src/components/sandbox.tsx/sandbox.test.tsx
@@ -1,17 +1,26 @@
-import { cleanup, render, screen } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+
 import Sandbox from '.';
 describe('Sandbox', () => {
     beforeEach(() => {
         render(<Sandbox />);
     });
-    afterEach(() => {
-        cleanup();
-    });
-
     it('should render text after 500ms', async () => {
         const asyncMessage = await screen.findByText('Async message');
         expect(asyncMessage).toBeInTheDocument();
+    });
+
+    it('must not return error', async () => {
+        const errorMessage = screen.queryByText('Error message');
+        expect(errorMessage).not.toBeInTheDocument();
+    });
+
+    it('must render 3 items', () => {
+    
+        const items = screen.getAllByText('Item 1');
+        expect(items).toHaveLength(3);
     });
 });
 
